afa776cf0e39568c0a1361f72dfd48ccaa45533d
ryanjhart-glitch <ryanjhart@gmail.com>
2026-03-11 09:40:35 -0700

    Add configuration file

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  searchKeywords: ['Veterinarian Medical Director', 'DVM Medical Director', 'Veterinary Clinical Director'],
+  locations: ['United States', 'California', 'Texas', 'New York', 'Florida'],
+  jobBoards: [
+    'linkedin.com',
+    'indeed.com',
+    'vetmedJobs.com',
+    'vetcareers.net'
+  ],
+  vetNetworks: [
+    'AAHA (American Animal Hospital Association)',
+    'AVMA (American Veterinary Medical Association)',
+    'LinkedIn Veterinary Groups'
+  ],
+  runInterval: 1440,
+  outputFormat: 'csv',
+  minExperience: 3,
+  saveLocation: './candidates.csv'
+};
